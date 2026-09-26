455c8f51e83d51f9d2c87f46618c58821f9fafd9
Kholis Dinu Syahputra <kholisdsyahschool@gmail.com>
2026-09-26 10:51:42 +0700

    Fix return statement indentation in Choice model

--- aplikasi/models.py
+++ aplikasi/models.py
@@ -2,21 +2,18 @@ from django.db import models
 
 class Course(models.Model):
     nama = models.CharField(max_length=200)
-
     def __str__(self):
         return self.nama
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     judul = models.CharField(max_length=200)
-
     def __str__(self):
         return self.judul
 
 class Question(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     teks = models.TextField()
-
     def __str__(self):
         return self.teks
 
@@ -24,6 +21,5 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     teks = models.CharField(max_length=200)
     benar = models.BooleanField(default=False)
-
     def __str__(self):
         return self.teks

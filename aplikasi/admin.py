e1b9fbc5b3b691787f9ee001eb6c6026867a5b28
Kholis Dinu Syahputra <kholisdsyahschool@gmail.com>
2026-09-26 19:37:36 +0700

    Refactor admin.py to include Instructor and Learner

--- aplikasi/admin.py
+++ aplikasi/admin.py
@@ -1,39 +1,42 @@
 from django.contrib import admin
-from .models import Course, Lesson, Question, Choice, Submission
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-class ChoiceInline(admin.TabularInline):
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
     model = Choice
-    extra = 3
+    extra = 2
 
-class QuestionInline(admin.TabularInline):
+
+class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
+
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('nama',)
-    search_fields = ('nama',)
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('judul', 'course')
-    list_filter = ('course',)
-    inlines = [QuestionInline]
+    list_display = ['title']
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('teks', 'lesson')
-    list_filter = ('lesson',)
     inlines = [ChoiceInline]
+    list_display = ['content']
 
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('teks', 'question', 'benar')
-    list_filter = ('benar', 'question')
-
-class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ('siswa', 'pertanyaan', 'tanggal_kirim')
-    list_filter = ('tanggal_kirim', 'siswa')
-    readonly_fields = ('tanggal_kirim',)
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice, ChoiceAdmin)
-admin.site.register(Submission, SubmissionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
